8065e006060d305d1ca6e4af8726650a46f9dbbf
spencer-dotnet <spencerdotnet@gmail.com>
2020-11-03 04:20:16 -0700

    Added Expense model

---
+++ Home/Shared/DAL/Models/Expense.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Home.Shared.DAL.Models
+{
+    public enum ExpenseCategory
+    {
+        Food,
+        Rent,
+        Utilities,
+        Bills,
+        Shopping,
+        Transportation,
+        Insurance,
+        Medical,
+        Clothing,
+        Tithing,
+        Charity,
+        Others,
+
+        Salary,
+        Interest,
+        Business,
+        Gifts
+    }
+
+    public enum ExpenseType
+    {
+        Income,
+        Expense
+    }
+
+    public class Expense
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public ExpenseCategory Category { get; set; }
+        public string PaymentMode { get; set; }
+        public ExpenseType Type { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+
+    }
+}
