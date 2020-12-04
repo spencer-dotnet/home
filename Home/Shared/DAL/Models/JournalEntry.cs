07dbce48af8c3119635519b565c6403ba7a350b7
spencer-dotnet <spencerdotnet@gmail.com>
2020-12-03 21:09:52 -0700

    update to net5.0

---
+++ Home/Shared/DAL/Models/JournalEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Home.Shared.DAL.Models
+{
+    public class JournalEntry
+    {
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+}
