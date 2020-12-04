2a985bc353a4076b432af24a37b89ad88dad1852
spencer-dotnet <spencerdotnet@gmail.com>
2020-12-03 21:27:16 -0700

    Completed migration to net5.0, removed MudBlazor

--- Home/Client/Program.cs
+++ Home/Client/Program.cs
@@ -17,7 +17,7 @@ namespace Home.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped<AppState>();
 
